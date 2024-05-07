06fe6051d7157ab8cf0b8c8cdc185fdca9f7187b
Robbin Xu <robbinxxy@gmail.com>
2024-05-07 20:03:18 +1200

    pixijs? how about no
    struggled with pixi js, not much done

--- src/game.js
+++ src/game.js
@@ -1,3 +1,5 @@
+import { player } from './player.js';
+
 const app = new PIXI.Application();
 await app.init({ width: window.innerWidth - window.innerHeight/20, height: window.innerHeight - window.innerHeight/20 })
 document.body.appendChild(app.canvas);
@@ -6,6 +8,10 @@ document.body.appendChild(app.canvas);
       await PIXI.Assets.load('https://pixijs.com/assets/files/sample-747abf529b135a1f549dff3ec846afbc.png');
       let sprite = PIXI.Sprite.from('https://pixijs.com/assets/files/sample-747abf529b135a1f549dff3ec846afbc.png');
       app.stage.addChild(sprite);
+
+      let playerCharacter = new player();
+      await playerCharacter.loadAssets();
+      app.stage.addChild(playerCharacter.animatedSprite);
 
       // Add a ticker callback to move the sprite back and forth
       let elapsed = 0.0;
