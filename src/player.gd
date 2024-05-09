c3832f4f4cc6048884d5f0bb769140fd2779e622
Newroll <75225538+Newroll@users.noreply.github.com>
2024-05-10 10:38:33 +1200

    added idle animation
    added idle and movement animations to the player character

--- src/player.gd
+++ src/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 var speed = 100 # defines the speed of the player
+var currentDirection = "down" # for animation purposes
 
 func _physics_process(delta):
+	basic_movement()
+	sprite_animation()
 
+func basic_movement():
 	# simple boilerplate code for basic movement w/o acceleration
 	var directionHorizontal = Input.get_axis("move_left", "move_right")
 	if directionHorizontal:
@@ -18,14 +22,27 @@ func _physics_process(delta):
 	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
 	
-	# sets animation of sprite
+
+func sprite_animation():
+	# sets animation of player sprite depending on velocity
 	if velocity.x < 0:
 		$move.set_animation("left")
+		currentDirection = "left"
 	if velocity.x > 0: 
 		$move.set_animation("right")
+		currentDirection = "right"
 	if velocity.y < 0:
 		$move.set_animation("up")
+		currentDirection = "up"
 	if velocity.y > 0:
 		$move.set_animation("down")
+		currentDirection = "down"
+	if velocity.y == 0 && velocity.x == 0:
+		$move.hide()
+		$idle.show()
+		$idle.set_animation(currentDirection)
+	else:
+		$move.show()
+		$idle.hide()
 
 	move_and_slide()
