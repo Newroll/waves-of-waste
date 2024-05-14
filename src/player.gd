fe34520adf332d901d8843d5d4c19e4341230cf6
Newroll <75225538+Newroll@users.noreply.github.com>
2024-05-14 15:07:36 +1200

    add acceleration
    diagonal movement is very broken still

--- src/player.gd
+++ src/player.gd
@@ -1,27 +1,50 @@
 extends CharacterBody2D
 
-var speed = 100 # defines the speed of the player
+const BASESPEED = 50 # defines base speed of the player
+const MAXSPEED = 100 # defines the max speed of the player
+const ACCELERATION = 1 # defines acceleration per frame
+const DECELERATION = 3 # same as above but deceleration
+
+var speed = 0 # defines the current speed of the player
+
 var currentDirection = "down" # for animation purposes
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	basic_movement()
 	sprite_animation()
+	print(speed)
+
+func handle_acceleration():
+	if speed < MAXSPEED:
+		speed += ACCELERATION
 
 func basic_movement():
-	# simple boilerplate code for basic movement w/o acceleration
+
 	var directionHorizontal = Input.get_axis("move_left", "move_right")
 	if directionHorizontal:
+		handle_acceleration()
 		velocity.x = directionHorizontal * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		if velocity.x > 0:
+			velocity.x -= DECELERATION
+		if velocity.x < 0:
+			velocity.x += DECELERATION
+		if velocity.x < 6 && velocity.x > -6 && velocity.x != 0:
+			velocity.x = 0
+			speed = 0
 	
 	var directionVertical = Input.get_axis("move_up", "move_down")
 	if directionVertical:
+		handle_acceleration()
 		velocity.y = directionVertical * speed
-		velocity.x = 0 # overrides horizontal movement to disable diagonal movement
 	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
-	
+		if velocity.y > 0:
+			velocity.y -= DECELERATION
+		if velocity.y < 0:
+			velocity.y += DECELERATION
+		if velocity.y < 6 && velocity.y > -6 && velocity.y != 0:
+			velocity.y = 0
+			speed = 0
 
 func sprite_animation():
 	# sets animation of player sprite depending on velocity
