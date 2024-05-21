88adcd6c27227377fc5cacd38fe3a499085fbcaa
Newroll <75225538+Newroll@users.noreply.github.com>
2024-05-21 14:41:57 +1200

    add the hud
    hud as pretty much the same functionality as the menu, with a few things changed. some menus have not been implemented yet

---
+++ src/hud.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+func _on_pause_pressed():
+	get_tree().paused = true
+	$pause.hide()
+	$hudopacity.show()
+
+# literally the same as the menu
+# this is a boolean value that sets the fullscreen status of the game
+func _on_fullscreen_toggled(toggled_on):
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	if toggled_on == false:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_settings_pressed():
+	pass # open the settings menu (not implemented atm)
+
+func _on_unpause_pressed():
+	get_tree().paused = false
+	$pause.show()
+	$hudopacity.hide()
+
+func _on_backmenu_pressed():
+	get_tree().paused = false
+	$pause.show()
+	$hudopacity.hide()
+	get_tree().change_scene_to_file("res://src/main-menu.tscn")
+
+func _on_helpmenu_pressed():
+	pass # open the help menu (not implemented atm)
