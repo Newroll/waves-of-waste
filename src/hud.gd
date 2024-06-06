281247a4f5b5cd2062ccf773ace98820ce9c7475
Robbin Xu <robbinxxy@gmail.com>
2024-06-06 20:38:53 +1200

    made hud bettuh

--- src/hud.gd
+++ src/hud.gd
@@ -1,9 +1,8 @@
 extends CanvasLayer
 
 func _process(_delta):
-	# sets text
-	$time.text = str(Main.formattedTime)
-	$points.text = str(Main.points)
+	# sets text, bit of string concactnation
+	$textLabel.text = str(Main.points) + "/" + str(Main.maxPoints[Main.currentLevel-1]) + " | " + str(Main.formattedTime)
 
 func _on_pause_pressed():
 	get_tree().paused = true
@@ -34,3 +33,9 @@ func _on_backmenu_pressed():
 
 func _on_helpmenu_pressed():
 	pass # open the help menu (not implemented atm)
+
+func _on_trash_button_pressed():
+	get_tree().paused = true
+	$pause.show()
+	$hudopacity.show()
+	Main.change_scene("res://src/trash_menu.tscn")
