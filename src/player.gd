73877f2aa7d53ed9cab3ab0877ce53fd43bb5344
Newroll <75225538+Newroll@users.noreply.github.com>
2024-08-02 18:57:01 +1200

    finish diagonal movement

--- src/player.gd
+++ src/player.gd
@@ -1,24 +1,21 @@
 extends CharacterBody2D
 
-const BASESPEED = 50 # defines base speed of the player
-const MAXSPEED = 100 # defines the max speed of the player
+const MAX_VELOCITY = 100 # defines the max velocity of the player
 const ACCELERATION = 2 # defines acceleration per frame
 const DECELERATION = 2 # same as above but deceleration
 
-var speed = Vector2(0 ,0) # the current speed of the player
-var theta
+var theta = 0
 
 func _physics_process(_delta):
 	var direction_vertical = Input.get_axis("move_up", "move_down")
 	var direction_horizontal = Input.get_axis("move_left", "move_right")
-	
+
 	# horizontal movement
 	if direction_horizontal: 
-		if speed.x < direction_horizontal * MAXSPEED:
-			speed.x += ACCELERATION
-		if speed.x > direction_horizontal * MAXSPEED:
-			speed.x -= ACCELERATION
-		velocity.x = speed.x
+		if velocity.x < direction_horizontal * MAX_VELOCITY:
+			velocity.x += ACCELERATION
+		if velocity.x > direction_horizontal * MAX_VELOCITY:
+			velocity.x -= ACCELERATION
 	else:
 		if velocity.x > 0:
 			velocity.x -= DECELERATION
@@ -26,15 +23,13 @@ func _physics_process(_delta):
 			velocity.x += DECELERATION
 		if velocity.x < 6 and velocity.x > - 6 and velocity.x != 0:
 			velocity.x = 0
-			speed.x = 0
 	
 	# vertical movement
 	if direction_vertical: 
-		if speed.y < direction_vertical * MAXSPEED:
-			speed.y += ACCELERATION
-		if speed.y > direction_vertical * MAXSPEED:
-			speed.y -= ACCELERATION
-		velocity.y = speed.y
+		if velocity.y < direction_vertical * MAX_VELOCITY:
+			velocity.y += ACCELERATION
+		if velocity.y > direction_vertical * MAX_VELOCITY:
+			velocity.y -= ACCELERATION
 	else:
 		if velocity.y > 0:
 			velocity.y -= DECELERATION
@@ -42,40 +37,10 @@ func _physics_process(_delta):
 			velocity.y += DECELERATION
 		if velocity.y < 6 and velocity.y > - 6 and velocity.y != 0:
 			velocity.y = 0
-			speed.y = 0
 	
-	# deceleration
-	#if not direction_vertical and not direction_horizontal:
-		#deceleration()
+	if velocity.length() > 0:
+		theta = velocity.angle()
 	
-	print(velocity)
-	#sprite_rotation()
+	$Sprite2D.rotation = theta
+	$CollisionShape2D.rotation = theta
 	move_and_slide()
-	
-func sprite_rotation():
-	# level 2 physics moment
-	# use inverse tangent to calculate the angle at which the boat is moving towards
-	theta = atan(abs(velocity.x)/abs(velocity.y))
-	if !is_nan(theta): # theta can be NaN if one of the arguments above is =0
-		# theta is locked between 0 and +pi/2 radians
-		# so we need to add/substract some radians to get the correct final angle
-		if velocity.x > 0:
-			if velocity.y > 0:
-				$Sprite2D.rotation = theta
-			if velocity.y < 0:
-				$Sprite2D.rotation = theta - PI/2
-		if velocity.x < 0:
-			if velocity.y > 0:
-				$Sprite2D.rotation = theta + PI/2
-			if velocity.y < 0:
-				$Sprite2D.rotation = theta - PI
-		if velocity.x == 0:
-			if velocity.y > 0:
-				$Sprite2D.rotation = PI/2
-			if velocity.y < 0:
-				$Sprite2D.rotation = -PI/2
-		if velocity.y == 0:
-			if velocity.x > 0:
-				$Sprite2D.rotation = 0
-			if velocity.x < 0:
-				$Sprite2D.rotation = PI
