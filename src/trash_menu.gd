ca4b8f5fa4adacef1361b8dae4d00a9c4e2e2330
Newroll <75225538+Newroll@users.noreply.github.com>
2024-08-23 17:04:04 +1200

    fix trash menu bug

--- src/trash_menu.gd
+++ src/trash_menu.gd
@@ -6,8 +6,11 @@ func _ready():
 	for i in Main.trash_list.size():
 		# duplicates the template container (templateContainer) as to how many trash there are
 		%VBoxContainer.add_child(%TemplateContainer.duplicate())
-		# kills the template because we don't need it anymore and it will mess things up
-		%TemplateContainer.queue_free()
+	# kills the template because we don't need it anymore and it will mess things up
+	%TemplateContainer.queue_free()
+
+func _process(_delta):
+	for i in Main.trash_list.size():
 		# sets sprites and text as to what is in the database
 		%VBoxContainer.get_child(i).get_child(0).get_child(0).get_child(0).set_texture(load("res://assets/trash/" + Main.trash_list[i].path))
 		%VBoxContainer.get_child(i).get_child(0).get_child(1).get_child(0).set_text(Main.trash_list[i].name)
@@ -16,8 +19,7 @@ func _ready():
 		if Main.trash_seen[i] == 0:
 			%VBoxContainer.get_child(i).get_child(0).get_child(1).get_child(0).set_text("???")
 			%VBoxContainer.get_child(i).get_child(0).get_child(1).get_child(1).set_text("This trash has not be discovered yet!\n\n\n")
-			%VBoxContainer.get_child(i).get_child(0).get_child(0).modulate = Color(0, 0, 0, 1)
-	%TemplateContainer.queue_free()
+			%VBoxContainer.get_child(i).get_child(0).get_child(0).get_child(0).modulate = Color(0, 0, 0, 1)
 
 func _input(event): # hotkey shortcut stuff
 	if event.is_action_pressed("escape"):
